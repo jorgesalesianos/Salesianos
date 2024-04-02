ce846ed04907c9d949235003ecb2b9fe328f0d50
jorgesalesianos <leon.pejor23@zaragoza.salesianos.edu>
2024-04-02 20:56:59 +0200

    Primer commit de prueba para Practica 1 de entornos

---
+++ Ejercicio1.java
@@ -0,0 +1,21 @@
+package actividad1;
+
+/**
+ * Implementa un programa Java que muestr por pantalla la palabra “Hola”
+ * y después, escribe mi nombre "Jorge" en la siguiente línea.
+ *
+ */
+public class Ejercicio1 {
+	
+	/**
+	 * Pres_ ---
+	 * Post: Escribe por pantalla una linea con el mensaje "Hola"
+	 * y despues, en la siguente linea escribe mi nombre, en este
+	 * caso "Jorge"
+	 */
+	public static void main (String[] args) {
+		System.out.println("Hola ");
+		System.out.print("Jorge");
+	}
+
+}
---
+++ Ejercicio2.java
@@ -0,0 +1,30 @@
+package actividad1;
+
+import java.util.Scanner;
+
+/**
+ * Implementa un programa Java que dados dos números enteros 
+ * (x, y) te muestre por pantalla la suma de ambos.
+ */
+public class Ejercicio2 {
+	
+	/**
+	 * Pre: ---
+	 * Post: Programa Java que dados dos números enteros (x, y) te 
+	 * muestra por pantalla la suma de ambos.
+	 */
+	public static void main(String[] args) {
+		int n1 = 99;
+		int n2 = 1;
+		int suma = n1+n2;
+		
+		System.out.println ( n1 + " + " + n2 + " = " + suma);
+	}
+	
+	
+}
+
+	
+	
+	
+	
